f6d794fac66d81fb76b4a183a9c90d211d1abfda
divinejoshua <divineuche399@gmail.com>
2024-06-24 00:42:39 +0100

    Application running

--- src/main/java/com/example/docker_java/DockerJavaApplication.java
+++ src/main/java/com/example/docker_java/DockerJavaApplication.java
@@ -1,7 +1,11 @@
 package com.example.docker_java;
 
+import com.example.docker_java.model.Post;
+import com.example.docker_java.repository.PostRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DockerJavaApplication {
@@ -9,5 +13,8 @@ public class DockerJavaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DockerJavaApplication.class, args);
 	}
-
+	@Bean
+	CommandLineRunner commandLineRunner(PostRepository repository) {
+		return args -> repository.save(new Post("Hello World!","My first blog post!"));
+	}
 }
--- src/main/java/com/example/docker_java/model/Post.java
+++ src/main/java/com/example/docker_java/model/Post.java
@@ -4,6 +4,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
 import java.util.Objects;
+
+@Entity
 public class Post {
     @Id
     @GeneratedValue
