82b08c867d39a4d6917d0e004aafe0685fa8748a
divinejoshua <divineuche399@gmail.com>
2024-06-24 09:43:21 +0100

    Removed database

--- src/main/java/com/example/docker_java/controller/PostController.java
+++ src/main/java/com/example/docker_java/controller/PostController.java
@@ -1,24 +1,28 @@
 package com.example.docker_java.controller;
-import com.example.docker_java.model.Post;
-import com.example.docker_java.repository.PostRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.HashMap;
+
 @RestController
-@RequestMapping("/api/posts")
+@RequestMapping("/api")
 public class PostController {
 
-    private final PostRepository repository;
 
-    public PostController(PostRepository repository) {
-        this.repository = repository;
+    public PostController() {
     }
 
-    @GetMapping
-    public List<Post> findAll() {
-        return repository.findAll();
+    @GetMapping("/user")
+    public ResponseEntity<HashMap<String, String>> getUserDetails() {
+        HashMap<String, String> userDetails = new HashMap<>();
+        userDetails.put("name", "Alice");
+        userDetails.put("email", "alice@example.com");
+        userDetails.put("age", "28");
+
+        return new ResponseEntity<>(userDetails, HttpStatus.OK);
     }
 
 }
--- src/main/java/com/example/docker_java/repository/PostRepository.java
+++
@@ -1,5 +0,0 @@
-package com.example.docker_java.repository;
-import com.example.docker_java.model.Post;
-import org.springframework.data.repository.ListCrudRepository;
-public interface PostRepository extends ListCrudRepository<Post,Integer> {
-}
